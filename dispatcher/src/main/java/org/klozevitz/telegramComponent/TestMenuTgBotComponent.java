e6f5f384ae53c210f188a3717589b7feb616e6fb
klozevitz <klozevitz@yandex.ru>
2024-12-07 16:12:16 +0300

    добавлен Lombok протестирован ответ
    TODO:
    Интеграция с RabbitMQ
    распределение сообщений по очередям RabbitMQ

--- dispatcher/src/main/java/org/klozevitz/telegramComponent/TestMenuTgBotComponent.java
+++ dispatcher/src/main/java/org/klozevitz/telegramComponent/TestMenuTgBotComponent.java
@@ -1,18 +1,20 @@
 package org.klozevitz.telegramComponent;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Log4j
 @Component
 public class TestMenuTgBotComponent extends TelegramLongPollingBot {
     @Value(value = "${bot.username}")
     private String username;
     @Value(value = "${bot.token}")
     private String token;
-    private static final Logger log = Logger.getLogger(TestMenuTgBotComponent.class);
 
     @Override
     public String getBotUsername() {
@@ -29,5 +31,22 @@ public class TestMenuTgBotComponent extends TelegramLongPollingBot {
         var message = update.getMessage();
         var textFromMessage = message.getText();
         log.debug(textFromMessage);
+
+        var responseText = String.format("ответ на сообщение \"%s\"", textFromMessage);
+
+        var response = new SendMessage();
+        response.setChatId(message.getChatId().toString());
+        response.setText(responseText);
+        sendAnswerMessage(response);
+    }
+
+    private void sendAnswerMessage(SendMessage message) {
+        if (message != null) {
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
